8291bdefeeaa9e6a8d9d84207902d7d8f853bfcc
Dark_Side <Dark_Side@DESKTOP-5KPKFJ8>
2016-11-14 19:11:06 -0600

    mi primer proyecto subido a github

---
+++ src/main/java/org/unitec/lenguajes/Prueba.java
@@ -0,0 +1,13 @@
+package org.unitec.lenguajes;
+
+/**
+ *
+ * @author Dark_Side
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        System.out.println("Probando github");
+        
+    }
+     
+}
